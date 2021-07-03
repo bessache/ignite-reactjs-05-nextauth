1920e9fa694b538fe7ca5d640bef42f7ddb2c0ea
Renato Bessa <bessache@outlook.com>
2021-07-03 10:27:34 -0300

    commit ajiste incio de projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/home.module.scss'
+
+
+export default function Home() {
+  return (
+    <div className={styles.contentContainer}>
+      <h1>Hello World</h1>
+    </div>
+    
+  )
+}
