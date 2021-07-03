76eafd6da59a57b94206d5823a0ca246db5b6310
Renato Bessa <bessache@outlook.com>
2021-07-03 11:20:54 -0300

    commit aula contexto de autenticação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
+import {AuthProvider} from '../contexts/AuthContext'
+
 import {AppProps} from 'next/app'
 
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }:AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
-
 export default MyApp
